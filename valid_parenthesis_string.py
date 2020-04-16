0b52f29452bb04ff1d86bdde75712d801d6fec10
Arunn Thevapalan <atarunn11@gmail.com>
2020-04-16 23:00:59 +0530

    Solve problem 16

---
+++ valid_parenthesis_string.py
@@ -0,0 +1,38 @@
+'''
+Problem 16 - 16/04/2020
+Given a string containing only three types of characters: '(', ')' and '*', write a function to check whether this string is valid. 
+We define the validity of a string by these rules:
+    Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+    Any right parenthesis ')' must have a corresponding left parenthesis '('.
+    Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+    '*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string.
+    An empty string is also valid.
+    string size would be in the range of [0,100]
+
+Example:
+Input: "(*))"
+Output: True
+'''
+#Approach - initialize 2 stack and pop only if indexes follow order
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        stack = []
+        star = []
+        for i, c in enumerate(s):
+            if c == '*':
+                star.append(i)
+            elif c=='(':
+                stack.append(i)
+            elif c==')':
+                if stack:
+                    stack.pop()
+                elif star:
+                    star.pop()
+                else: return False
+                
+        while star and stack:
+            if (stack[-1] < star[-1]):
+                stack.pop()
+                star.pop()
+            else: break
+        return len(stack)==0
